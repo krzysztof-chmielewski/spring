edf6fb14fb4ba15e48fe4c4c0627bcb638f0473c
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-06-27 18:52:28 +0200

    Injecting ApplicationCounter instead of calling ApplicationContext to fetch it

--- Counter/src/main/java/com/kchmielewski/spring/counter/logic/VisitCounterService.java
+++ Counter/src/main/java/com/kchmielewski/spring/counter/logic/VisitCounterService.java
@@ -1,6 +1,7 @@
 package com.kchmielewski.spring.counter.logic;
 
 import com.kchmielewski.spring.counter.model.ApplicationCounter;
+import com.kchmielewski.spring.counter.model.Counter;
 import com.kchmielewski.spring.counter.model.SessionCounter;
 import com.kchmielewski.spring.counter.web.VisitCounterResponse;
 import org.springframework.context.ApplicationContext;
@@ -9,18 +10,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class VisitCounterService {
     private final ApplicationContext applicationContext;
+    private final Counter applicationCounter;
 
-    public VisitCounterService(ApplicationContext applicationContext) {
+    public VisitCounterService(ApplicationContext applicationContext, ApplicationCounter counter) {
         this.applicationContext = applicationContext;
+        applicationCounter = counter;
     }
 
     public VisitCounterResponse count() {
-        return new VisitCounterResponse(applicationContext.getBean(ApplicationCounter.class).count(),
+        return new VisitCounterResponse(applicationCounter.count(),
                 applicationContext.getBean(SessionCounter.class).count());
     }
 
     public VisitCounterResponse visit() {
-        return new VisitCounterResponse(applicationContext.getBean(ApplicationCounter.class).visit(),
+        return new VisitCounterResponse(applicationCounter.visit(),
                 applicationContext.getBean(SessionCounter.class).visit());
     }
 }
